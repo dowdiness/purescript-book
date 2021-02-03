1fdf15f471243975d1d73118e39fbc502dba8685
Koji Ishimoto <koji.ishimoto@gmail.com>
2021-02-03 23:29:20 +0900

    Finished chapter2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,15 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Data.Int (rem)
+import Math (sqrt, pi)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea radius = radius * radius * pi
+
+leftoverCents :: Int -> Int
+leftoverCents i = rem i 100
